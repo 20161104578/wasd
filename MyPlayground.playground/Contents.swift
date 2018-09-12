9c2e52733b63d59a7d1723d333803ade3f07472c
20161104578 <a20161104578@chaolimengdeiMac.local>
2018-09-12 11:26:50 +0800

    add
    add

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var x = 0
+var y = 0
+for x in 0...100{
+  x = x + y
+}
---
+++ os/os/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  os
+//
+//  Created by 20161104578 on 18/9/12.
+//  Copyright © 2018年 20161104578. All rights reserved.
+//
+
+import Foundation
+
+var x = 0
+var y = 0
+for x in 0...100
+{
+   y = y+x
+}
+print(y)
+
